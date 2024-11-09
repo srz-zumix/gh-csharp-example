e3140422c517d14afa3836af5f026fc738a4fbc5
srz_zumix <zumix.cpp@gmail.com>
2024-11-09 22:23:45 +0900

    fix

--- script/build.sh
+++ script/build.sh
@@ -14,7 +14,7 @@ dotnet restore "${SLN}"
 dotnet build "${SLN}"
 
 function publish() {
-    dotnet publish "${SLN}" -c Release -r "$1" -o "${OUTPUTDIR}/$1" \
+    dotnet publish "${SLN}" -c Release -r "$1" --property:PublishDir="${OUTPUTDIR}/$1" \
         --no-restore --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
     if [ -f "${OUTPUTDIR}/$1/${COMMAND_NAME}.exe" ]; then
         cp "${OUTPUTDIR}/$1/${COMMAND_NAME}.exe" "dist/${COMMAND_NAME}-$2.exe"
@@ -28,4 +28,4 @@ function publish() {
 publish osx-x64   darwin-amd64
 publish osx-arm64 darwin-arm64
 publish linux-x64 linux-amd64
-publish win10-x64 windows-amd64
+publish win-x64 windows-amd64
