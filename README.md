6e0af8178dbefbccad58d5f87065437c2207beaf
srz_zumix <zumix.cpp@gmail.com>
2024-11-09 22:48:38 +0900

    first (#1)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gh-csharp-example
+
+gh extension C# example
+
+## Installation
+
+```sh
+gh extension install srz-zumix/gh-csharp-example
+```
---
+++ gh-csharp-example.Test/TestCase/SampleTest.cs
@@ -0,0 +1,10 @@
+namespace temp.Test.TestCase;
+
+public class SampleTest
+{
+    [Test]
+    public void Pass()
+    {
+        Assert.IsTrue(true);
+    }
+}
